d2ad5508b08d1d976cb87fc21cfbba91e2b19358
Daniel Rutschmann <daniel.rutschmann@gmx.ch>
2018-01-13 20:41:43 +0100

    add #pragma directives usable to speed up code

---
+++ utility/pragma_speedup.cpp
@@ -0,0 +1,4 @@
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")

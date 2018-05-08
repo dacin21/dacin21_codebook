86c85ff69f41546241f1697e06e5134015940fb2
Daniel Rutschmann <daniel.rutschmann@gmx.ch>
2018-05-08 13:36:49 +0200

    add pragmas that work on yandex

--- utility/pragma_speedup.cpp
+++ utility/pragma_speedup.cpp
@@ -1,4 +1,5 @@
 #pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native") // codeforces
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,tune=native") // yandex

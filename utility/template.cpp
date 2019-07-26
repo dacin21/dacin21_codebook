9214780bb885f6ff3a19f337ceaeb9cfb9dfdd5b
Daniel Rutschmann <daniel.rutschmann@gmx.ch>
2019-07-26 12:55:27 +0200

    update template. Added debug struct. Added min_heap. Fixed unused vars warnings in gen().

--- utility/template.cpp
+++ utility/template.cpp
@@ -1,19 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 using ll = long long;
+using ull = unsigned long long;
+using fl = long double;
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
 template<typename S, typename T>
 void xmin(S&a, T const&b){if(b<a) a=b;}
 template<typename S, typename T>
 void xmax(S&a, T const&b){if(b>a) a=b;}
 
+template<bool enabled>
+struct Debug{
+    template<typename S, typename T = void> struct Tag_Printable : false_type {};
+    template<typename S> struct Tag_Printable<S, decltype((void)(cerr << declval<S>()))> : true_type {};
+    template<typename S, typename T = void> struct Tag_Iterable: false_type {};
+    template<typename S> struct Tag_Iterable<S, decltype((void)(begin(declval<S>()), end(declval<S>())))> : true_type {};
+    template<typename T, typename... Args>
+    Debug& print(T const&x, true_type, Args...){
+        #ifdef LOCAL_RUN
+        if(enabled){
+            cerr << boolalpha << x;
+        }
+        #endif // LOCAL_RUN
+        return *this;
+    }
+    template<typename T>
+    Debug& print(T const&x, false_type, true_type){
+        *this << "[";
+        bool first = true;
+        for(auto &e:x){
+            if(!first) *this << ", ";
+            *this << e;
+            first = false;
+        }
+        return *this << "]";
+    }
+    template<typename S, typename T>
+    Debug& print(pair<S, T> const&x, false_type, false_type){
+        return *this << "(" << x.first << ", " << x.second << ")";
+    }
+    template<typename T>
+    Debug& operator<<(T const&x){
+        return print(x, Tag_Printable<T>{}, Tag_Iterable<T>{});
+    }
+};
+ Debug<true> debug;
+// Debug<false> debug; // disable debug printing
+#define named(x) string(#x) << " : " <<  x
+
+
+
+void solve(){
+    /// SOLVE HERE
+
+
+}
+
 signed gen(int T){
     mt19937 rng(43151);
     auto get_rand = [&](int64_t l, int64_t r){
         return uniform_int_distribution<int64_t>(l, r)(rng);
-    };
+    }; (void) get_rand;
     auto get_double = [&](double l, double r){
         return uniform_real_distribution<double>(l, r)(rng);
-    };
+    };  (void) get_double;
     ofstream o("gen.txt");
     o << T << "\n";
     for(int cas=0;cas<T;++cas){
@@ -31,18 +83,19 @@ signed main()
     #ifdef LOCAL_RUN
     freopen("in.txt", "r", stdin);
     //freopen("out.txt", "w", stdout);
-    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
-    int TTT; cin >> TTT; 
-	if(TTT < 0) return gen(-TTT);
-	while(TTT--){
+    cin.exceptions(ios::badbit | ios::eofbit | ios::failbit);
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    int TTT; cin >> TTT;
+    if(TTT < 0) return gen(-TTT);
+    while(TTT--){
     #else
-    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
     #endif // LOCAL_RUN
 
-    ///CODE
-
+    solve();
 
     #ifdef LOCAL_RUN
+    cout << flush;
     }
     #endif // LOCAL_RUN
     return 0;

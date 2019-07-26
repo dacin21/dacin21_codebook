6d6c89e03b8f732b11f4db6d00cbd9e28b912f6a
Daniel Rutschmann <daniel.rutschmann@gmx.ch>
2019-07-26 12:52:56 +0200

    add additional pragmas

--- utility/pragma_speedup.cpp
+++ utility/pragma_speedup.cpp
@@ -2,4 +2,5 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native") // codeforces
+//#pragma GCC target("avx,avx2,fma")
 //#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,tune=native") // yandex

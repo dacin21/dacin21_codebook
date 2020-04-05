7c61aef6c40f5bccbce7a5ab83da9b3cc6e8d7bf
Daniel Rutschmann <daniel.rutschmann@gmx.ch>
2020-04-05 21:03:41 +0200

    improve debug printing in template.

--- utility/template.cpp
+++ utility/template.cpp
@@ -11,44 +11,144 @@ void xmin(S&a, T const&b){if(b<a) a=b;}
 template<typename S, typename T>
 void xmax(S&a, T const&b){if(b>a) a=b;}
 
-template<bool enabled>
-struct Debug{
-    template<typename S, typename T = void> struct Tag_Printable : false_type {};
-    template<typename S> struct Tag_Printable<S, decltype((void)(cerr << declval<S>()))> : true_type {};
-    template<typename S, typename T = void> struct Tag_Iterable: false_type {};
-    template<typename S> struct Tag_Iterable<S, decltype((void)(begin(declval<S>()), end(declval<S>())))> : true_type {};
-    template<typename T, typename... Args>
-    Debug& print(T const&x, true_type, Args...){
-        #ifdef LOCAL_RUN
-        if(enabled){
-            cerr << boolalpha << x;
-        }
-        #endif // LOCAL_RUN
-        return *this;
+
+
+namespace debug_{
+
+template<size_t N> struct priority : priority<N-1> {};
+template<> struct priority<0> {};
+
+template<bool enabled> struct Debug;
+template<typename T, typename SFINAE = void> struct Printer : enable_if<false> {};
+// pair and tuple
+template<typename S, typename T>
+struct Printer<pair<S, T> > : enable_if<true> {
+    template<typename D>
+    static void print(D const&d, pair<S, T> const&val){
+        d << "(" << val.first << ", " << val.second << ")";
     }
-    template<typename T>
-    Debug& print(T const&x, false_type, true_type){
+};
+template<typename... S>
+struct Printer<tuple<S ...> > : enable_if<true> {
+    template<size_t i, size_t n, typename D>
+    static void print_rec(D const&d, tuple<S ...> const&val){
+        d << get<i>(val);
+        if(i+1 < n) {
+            d << ", ";
+            print_rec<i+(i+1<n), n>(d, val);
+        }
+    }
+    template<typename D>
+    static void print(D const&d, tuple<S ...> const&val){
+        d << "(";
+        print_rec<0, tuple_size<tuple<S...> >::value >(d, val);
+        d << ")";
+    }
+};
+template<>
+struct Printer<tuple<> > : enable_if<true> {
+    template<typename D>
+    static void print(D const&d, tuple<> const&){
+        d << "()";
+    }
+};
+// non-iterable container
+template<typename T>
+struct Hacked_Container : T{
+    static auto get(T const& val){
+        return val.*&Hacked_Container::c;
+    }
+};
+template<typename T> struct is_hackable_container : false_type{};
+template<typename... T> struct is_hackable_container<stack<T...> > : true_type{};
+template<typename... T> struct is_hackable_container<queue<T...> > : true_type{};
+template<typename... T> struct is_hackable_container<priority_queue<T...> > : true_type{};
+template<typename T>
+struct Printer<T, enable_if_t<is_hackable_container<T>::value> >  : enable_if<true> {
+    template<typename D>
+    static void print(D const&d, T const&s){
+        d << Hacked_Container<T>::get(s);
+    }
+};
+// custom printable struct
+template<typename T>
+struct Printer<T,enable_if_t<sizeof((declval<T>().debug_print_(2), 1)) != -1>> : enable_if<true>{
+    template<typename D>
+    static void print(D const&d, T const&val){
+        val.debug_print_(d);
+    }
+};
+
+template <>
+struct Debug<false>{
+    template<typename T, typename = enable_if_t<is_same<int, decltype((declval<Debug<true>>() << declval<T>()), (int)1)>::value > >
+    friend const Debug& operator<<(Debug const&o, T const&){
+        return o;
+    }
+    template<typename... Args>
+    void pprint_all(string const&names, Args const& ... vals){}
+};
+template <>
+struct Debug<true>{
+    template<typename T, typename = decltype(cerr << declval<T>())>
+    void print(T const&val, priority<30>) const {
+        cerr << val;
+    }
+    template<typename T, typename = decltype((begin(declval<T>()), end(declval<T>())))>
+    void print(T const&val, priority<20>) const {
         *this << "[";
         bool first = true;
-        for(auto &e:x){
+        for(auto const&e:val){
             if(!first) *this << ", ";
             *this << e;
             first = false;
         }
-        return *this << "]";
+        *this << "]";
     }
-    template<typename S, typename T>
-    Debug& print(pair<S, T> const&x, false_type, false_type){
-        return *this << "(" << x.first << ", " << x.second << ")";
+    template<typename T, typename = typename Printer<T>::type>
+    void print(T const&val, priority<50>) const {
+        Printer<T>::print(*this, val);
     }
-    template<typename T>
-    Debug& operator<<(T const&x){
-        return print(x, Tag_Printable<T>{}, Tag_Iterable<T>{});
+    void pprint_all_rec(string const&, size_t) const {}
+    template<typename T, typename... Args>
+    void pprint_all_rec(string const&names, size_t pos, T const&val, Args const& ... rest) const {
+        if(pos != 0) *this << ", ";
+        assert(pos != string::npos);
+        size_t next = names.find(",", pos);
+        if(next == string::npos){
+                *this << names.substr(pos);
+        } else {
+            *this << names.substr(pos, next-pos);
+            ++next;
+        }
+        *this << "=" << val;
+        pprint_all_rec(names, next, rest...);
+    }
+
+    template<typename... Args>
+    void pprint_all(string const&names, Args const& ... vals) const {
+        *this << "(";
+        pprint_all_rec(names, 0, vals...);
+        *this << ")";
+    }
+    template<typename T, typename = decltype(declval<Debug>().print(declval<T>(), priority<99>{}))>
+    friend const Debug& operator<<(Debug const &o, T const& val){
+        o.print(val, priority<99>{});
+        return o;
     }
 };
- Debug<true> debug;
-// Debug<false> debug; // disable debug printing
+}
+
+#define make_printable(...)\
+template<typename T>\
+void debug_print_(T const& d)const{\
+    d.pprint_all(#__VA_ARGS__, __VA_ARGS__);\
+}
+
+ debug_::Debug<true> debug;
+// debug_::Debug<false> debug; // disable debug printing
 #define named(x) string(#x) << " : " <<  x
+
 
 
 
--- utility/template_codejam.cpp
+++ utility/template_codejam.cpp
@@ -1,18 +1,160 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 using ll = long long;
 using ull = unsigned long long;
 using fl = long double;
-
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
 template<typename S, typename T>
 void xmin(S&a, T const&b){if(b<a) a=b;}
 template<typename S, typename T>
 void xmax(S&a, T const&b){if(b>a) a=b;}
 
-using namespace std;
+
+
+namespace debug_{
+
+template<size_t N> struct priority : priority<N-1> {};
+template<> struct priority<0> {};
+
+template<bool enabled> struct Debug;
+template<typename T, typename SFINAE = void> struct Printer : enable_if<false> {};
+// pair and tuple
+template<typename S, typename T>
+struct Printer<pair<S, T> > : enable_if<true> {
+    template<typename D>
+    static void print(D const&d, pair<S, T> const&val){
+        d << "(" << val.first << ", " << val.second << ")";
+    }
+};
+template<typename... S>
+struct Printer<tuple<S ...> > : enable_if<true> {
+    template<size_t i, size_t n, typename D>
+    static void print_rec(D const&d, tuple<S ...> const&val){
+        d << get<i>(val);
+        if(i+1 < n) {
+            d << ", ";
+            print_rec<i+(i+1<n), n>(d, val);
+        }
+    }
+    template<typename D>
+    static void print(D const&d, tuple<S ...> const&val){
+        d << "(";
+        print_rec<0, tuple_size<tuple<S...> >::value >(d, val);
+        d << ")";
+    }
+};
+template<>
+struct Printer<tuple<> > : enable_if<true> {
+    template<typename D>
+    static void print(D const&d, tuple<> const&){
+        d << "()";
+    }
+};
+// non-iterable container
+template<typename T>
+struct Hacked_Container : T{
+    static auto get(T const& val){
+        return val.*&Hacked_Container::c;
+    }
+};
+template<typename T> struct is_hackable_container : false_type{};
+template<typename... T> struct is_hackable_container<stack<T...> > : true_type{};
+template<typename... T> struct is_hackable_container<queue<T...> > : true_type{};
+template<typename... T> struct is_hackable_container<priority_queue<T...> > : true_type{};
+template<typename T>
+struct Printer<T, enable_if_t<is_hackable_container<T>::value> >  : enable_if<true> {
+    template<typename D>
+    static void print(D const&d, T const&s){
+        d << Hacked_Container<T>::get(s);
+    }
+};
+// custom printable struct
+template<typename T>
+struct Printer<T,enable_if_t<sizeof((declval<T>().debug_print_(2), 1)) != -1>> : enable_if<true>{
+    template<typename D>
+    static void print(D const&d, T const&val){
+        val.debug_print_(d);
+    }
+};
+
+template <>
+struct Debug<false>{
+    template<typename T, typename = enable_if_t<is_same<int, decltype((declval<Debug<true>>() << declval<T>()), (int)1)>::value > >
+    friend const Debug& operator<<(Debug const&o, T const&){
+        return o;
+    }
+    template<typename... Args>
+    void pprint_all(string const&names, Args const& ... vals){}
+};
+template <>
+struct Debug<true>{
+    template<typename T, typename = decltype(cerr << declval<T>())>
+    void print(T const&val, priority<30>) const {
+        cerr << val;
+    }
+    template<typename T, typename = decltype((begin(declval<T>()), end(declval<T>())))>
+    void print(T const&val, priority<20>) const {
+        *this << "[";
+        bool first = true;
+        for(auto const&e:val){
+            if(!first) *this << ", ";
+            *this << e;
+            first = false;
+        }
+        *this << "]";
+    }
+    template<typename T, typename = typename Printer<T>::type>
+    void print(T const&val, priority<50>) const {
+        Printer<T>::print(*this, val);
+    }
+    void pprint_all_rec(string const&, size_t) const {}
+    template<typename T, typename... Args>
+    void pprint_all_rec(string const&names, size_t pos, T const&val, Args const& ... rest) const {
+        if(pos != 0) *this << ", ";
+        assert(pos != string::npos);
+        size_t next = names.find(",", pos);
+        if(next == string::npos){
+                *this << names.substr(pos);
+        } else {
+            *this << names.substr(pos, next-pos);
+            ++next;
+        }
+        *this << "=" << val;
+        pprint_all_rec(names, next, rest...);
+    }
+
+    template<typename... Args>
+    void pprint_all(string const&names, Args const& ... vals) const {
+        *this << "(";
+        pprint_all_rec(names, 0, vals...);
+        *this << ")";
+    }
+    template<typename T, typename = decltype(declval<Debug>().print(declval<T>(), priority<99>{}))>
+    friend const Debug& operator<<(Debug const &o, T const& val){
+        o.print(val, priority<99>{});
+        return o;
+    }
+};
+}
+
+#define make_printable(...)\
+template<typename T>\
+void debug_print_(T const& d)const{\
+    d.pprint_all(#__VA_ARGS__, __VA_ARGS__);\
+}
+
+ debug_::Debug<true> debug;
+// debug_::Debug<false> debug; // disable debug printing
+#define named(x) string(#x) << " : " <<  x
+
+
 
 
 void solve(){
     /// SOLVE HERE
+
 
 }
 
@@ -20,10 +162,10 @@ signed gen(int T){
     mt19937 rng(43151);
     auto get_rand = [&](int64_t l, int64_t r){
         return uniform_int_distribution<int64_t>(l, r)(rng);
-    };
+    }; (void) get_rand;
     auto get_double = [&](double l, double r){
         return uniform_real_distribution<double>(l, r)(rng);
-    };
+    }; (void) get_double;
     ofstream o("gen.txt");
     o << T << "\n";
     for(int cas=0;cas<T;++cas){
@@ -41,6 +183,7 @@ int main()
     #ifdef LOCAL_RUN
     freopen("inX.txt", "r", stdin);
     //freopen("outX.txt", "w", stdout);
+    cin.exceptions(ios::badbit | ios::eofbit | ios::failbit);
     #endif // LOCAL_RUN
     cin.tie(0); cout.tie(0);
     ios_base::sync_with_stdio(false);

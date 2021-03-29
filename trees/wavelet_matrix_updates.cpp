c80deaf1d076d663ab3e4eeced563d825a9dc765
Daniel Rutschmann <daniel.rutschmann@gmx.ch>
2021-03-29 13:40:37 +0200

    add wavelet tree with offline updates

--- trees/wavelet_matrix_updates.cpp
+++ trees/wavelet_matrix_updates.cpp
@@ -1,10 +1,100 @@
 /*
  *  Wavelet tree with low memory usage.
- *  Supports several orthogonal queries in O(log n)
- *  Paper: Wavelet Trees for Competitive Programming, ioiconf16
+ *  Supports several orthogonal queries in O((log n)^2)
+ *
+ *  Also supports activate / deactivate updates, which can
+ *  be used for other types of updates in an offline setting.
  *
  *  This implementation with popcount is inspired by anta's WaveletMatrix.
  */
+
+class Bit_Bit{
+public:
+    static constexpr uint32_t omega = CHAR_BIT * sizeof(uint64_t);
+    static constexpr uint32_t lg_omega = __lg(omega);
+    static_assert(omega == 64u);
+
+    Bit_Bit(vector<uint64_t> mask_) : n(mask_.size()), mask(move(mask_)), fenwick(n+1){
+        build();
+    }
+    Bit_Bit(uint32_t bits, bool init_val = 0) : n((bits>>lg_omega) + 1), mask(n, init_val ? ~uint64_t{0} : uint64_t{0}), fenwick(n+1){
+        if(init_val) mask.back()<<=((n<<lg_omega) - bits);
+        build();
+    }
+    /// popcount l <= i < r
+    uint32_t query(uint32_t l, uint32_t r) const {
+        if(__builtin_expect(r < l, false)) return 0;
+        return query(r) - query(l);
+    }
+    /// popcount 0 <= i < x
+    uint32_t query(uint32_t x) const {
+        uint32_t high = x>>lg_omega, low = x & ((uint64_t{1}<<lg_omega) - 1);
+        uint32_t ret = fenwick_query(high);
+        ret+= __builtin_popcountll(mask[high]& ((uint64_t{1} << low)-1));
+        return ret;
+    }
+    /// set x-th bit to val
+    void update(uint32_t x, bool val){
+        uint32_t high = x>>lg_omega, low = x & ((1u<<lg_omega) - 1);
+        if(((mask[high] >> low) & 1) != val){
+            mask[high]^= uint64_t{1}<<low;
+            fenwick_update(high, val ? 1 : -1);
+        }
+    }
+
+    void update_pre_build(uint32_t x, bool val){
+        uint32_t high = x>>lg_omega, low = x & ((1u<<lg_omega) - 1);
+        mask[high] = (mask[high] & ~(uint64_t{1} << low)) | (uint64_t{val}<<low);
+    }
+    void do_build(){
+        build();
+    }
+
+    friend ostream& operator<<(ostream&o, Bit_Bit const&b){
+        for(auto const&e:b.mask){
+            stringstream ss;
+            ss << bitset<omega>(e);
+            auto s = ss.str();
+            reverse(s.begin(), s.end());
+            o << s << "|";
+        }
+        o << " : ";
+        for(auto const&e:b.fenwick) o << e << " ";
+        o << "\n";
+        return o;
+    }
+
+private:
+    void fenwick_build(){
+        for(uint32_t x=1;x<=n;++x){
+            uint32_t y = x+(x&-x);
+            if(y < n) fenwick[y]+=fenwick[x];
+        }
+    }
+    void fenwick_update(uint32_t x, uint32_t val){
+        for(++x;x<=n;x+=x&-x){
+            fenwick[x]+=val;
+        }
+    }
+    // sum 0 <= i < x
+    uint32_t fenwick_query(uint32_t x) const {
+        uint32_t ans = 0;
+        for(;x;x-=x&-x){
+            ans+=fenwick[x];
+        }
+        return ans;
+    }
+    void build(){
+        for(uint32_t x=0;x<n;++x){
+            fenwick[x+1] = __builtin_popcountll(mask[x]);
+        }
+        fenwick_build();
+    }
+
+    const uint32_t n;
+    vector<uint64_t> mask;
+    vector<uint32_t> fenwick;
+};
 
 class Bit_Presum{
 public:
@@ -82,7 +172,7 @@ public:
     static_assert(is_integral<T>::value);
     static constexpr uint32_t height = CHAR_BIT * sizeof(T);
 
-    Wavelet_Matrix(vector<T> v): n(v.size()), data(height, n){
+    Wavelet_Matrix(vector<T> v): n(v.size()), data(height, n), active(height, n){
         build(move(v));
     }
     /// count l <= i < r  s.t.  A <= val[i] < B
@@ -90,15 +180,31 @@ public:
         assert(0 <= l && r <= n);
         return count_lower(l, r, B) - count_lower(l, r, A);
     }
-    /// count l <= i < r  s.t.  A <= val[i]
-    uint32_t range_count_up(int const&l, int const&r, T const&A) const {
-        assert(0 <= l && r <= n);
-        if(__builtin_expect(l>r, false)) return uint32_t{0};
-        return (r-l) - count_lower(l, r, A);
-    }
     T k_th(int const&l, int const&r, int k) const {
         assert(0 <= k && k < n);
         return get_kth(l, r, k);
+    }
+    T k_th_active(int const&l, int const&r, int k) const {
+        assert(0 <= k && k < n);
+        return get_kth_active(l, r, k);
+    }
+
+    void update_activity(int x, bool val){
+        assert(0 <= x && x < n);
+        int h = height;
+        T a = numeric_limits<T>::min();
+        while(h > 0){
+            --h;
+            const int low_l = data[h].query(x), low_r = data[h].query(x+1);
+            bool go_left = low_r!=low_l;
+            if(go_left){
+                active[h].update(x, val);
+                x = low_l;
+            } else {
+                a = a + (T{1}<<(max(0, h-1))) - !h + (T{1}<<(max(0, h-1)));
+                x = m_index[h] + x-low_l;
+            }
+        }
     }
 
 private:
@@ -111,6 +217,7 @@ private:
                 data[h].update_pre_build(i, v[i]<b);
             }
             data[h].do_build();
+            active[h].do_build();
             const int m = stable_partition(v.begin(), v.end(), [&b](T const&x){return x < b;}) - v.begin();
             for(int i=m;i<n;++i){
                 v[i] = v[i] - (T{1}<<(max(0, h-1))) + !h - (T{1}<<(max(0, h-1)));
@@ -118,7 +225,6 @@ private:
             m_index[h] = m;
         }
     }
-    /// count l <= i < r  s.t.  val[i] < B
     uint32_t count_lower(int l, int r, T const&B) const {
         assert(0 <= l && r <= n);
         if(__builtin_expect(r<l, false)) return 0;
@@ -127,14 +233,15 @@ private:
         T a = numeric_limits<T>::min();
         while(h > 0){
             --h;
-			bool go_left = B < a + (T{1}<<(max(0, h-1))) - !h + (T{1}<<(max(0, h-1)));
+			bool go_left = B <= a + (T{1}<<(max(0, h-1))) - !h + (T{1}<<(max(0, h-1)));
             const int low_l = data[h].query(l), low_r = data[h].query(r);
+            const int act_l = active[h].query(l), act_r = active[h].query(r);
             if(go_left){
                 l = low_l;
                 r = low_r;
             } else {
                 a = a + (T{1}<<(max(0, h-1))) - !h + (T{1}<<(max(0, h-1)));
-                ret+= low_r-low_l;
+                ret+= act_r-act_l;
                 l = m_index[h] + l-low_l;
                 r = m_index[h] + r-low_r;
             }
@@ -154,8 +261,31 @@ private:
                 l = low_l;
                 r = low_r;
             } else {
-                a+= T{1}<<h;
+                a = a + (T{1}<<(max(0, h-1))) - !h + (T{1}<<(max(0, h-1)));
                 k-= low_lr;
+                l = m_index[h] + l-low_l;
+                r = m_index[h] + r-low_r;
+            }
+        }
+        return a;
+    }
+    T get_kth_active(int l, int r, int k) const {
+        assert(0 <= l && r <= n);
+        assert(0 <= k && k < r-l);
+        int h = height;
+        T a = numeric_limits<T>::min();
+        while(h > 0){
+            --h;
+            const int low_l = data[h].query(l), low_r = data[h].query(r);
+            const int act_l = active[h].query(l), act_r = active[h].query(r);
+            const int act_lr = act_r-act_l;
+            bool go_left = k < act_lr;
+            if(go_left){
+                l = low_l;
+                r = low_r;
+            } else {
+                a = a + (T{1}<<(max(0, h-1))) - !h + (T{1}<<(max(0, h-1)));
+                k-= act_lr;
                 l = m_index[h] + l-low_l;
                 r = m_index[h] + r-low_r;
             }
@@ -166,4 +296,5 @@ private:
     const int n;
     vector<int> m_index;
     vector<Bit_Ds> data;
+    vector<Bit_Bit> active;
 };
